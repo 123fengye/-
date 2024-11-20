472f69a721eda0115f5d0630056838f4506bce5e
周润乐 <@2643811940qq.com>
2024-11-20 19:26:02 +0800

    添加功能：预算管理、月度统计

--- Cust.java
+++ Cust.java
@@ -1,12 +1,16 @@
-package Cust;
-
 import java.util.Scanner;
+import static java.lang.System.exit;
+
 public class Cust {
-    public int money;//余额
-    public int count;
-    public String time;
-    public String remark;
-    public String sort;
+    public int money;  //金额
+    public int count;  //余额
+    public String time;  //时间
+    public String remark; //备注
+    public String sort;   //类别
+    public int months;   //月份
+
+    public static int[] income_month = new int[12];  //每个月份的收入
+    public static int[] output_month = new int[12];
 
     public Cust(){
 
@@ -53,7 +57,6 @@ public class Cust {
           5. 设置月度预算（第五周任务添加）
           6. 查看月度统计报告（第五周任务添加）
           7. 退出系统*/
-
     }
 
     // 记录收入信息
@@ -61,10 +64,12 @@ public class Cust {
         System.out.println("请输入收入信息：");
         System.out.print("日期（YYYY-MM-DD）：");
         arr[index].time = sc.next();
+        arr[index].months = Integer.parseInt(arr[index].time.substring(5, 7)) - 1;
         System.out.print("金额：");
         while (true) {
             arr[index].money = sc.nextInt();
             if (arr[index].money >= 0) {
+                income_month[months] += arr[index].money ;
                 break;
             } else {
                 System.out.println("金额错误！");
@@ -82,10 +87,12 @@ public class Cust {
         System.out.println("请输入支出信息：");
         System.out.print("日期（YYYY-MM-DD）：");
         arr[index].time = sc.next();
+        arr[index].months = Integer.parseInt(arr[index].time.substring(5, 7)) - 1;
         System.out.print("金额：");
         while (true) {
             arr[index].money = sc.nextInt();
             if (arr[index].money >= 0) {
+                output_month[months] += arr[index].money ;
                 break;
             } else {
                 System.out.println("金额错误！");
@@ -98,6 +105,7 @@ public class Cust {
         System.out.println("支出已成功记录！");
     }
 
+
     // 打印所有记录
     public void print(Cust[] arr, int count) {
         System.out.println("日期（YYYY-MM-DD）："+ time);
@@ -109,9 +117,11 @@ public class Cust {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Cust[] array = new Cust[100];
+        Cust[] array = new Cust[100];   //收入
+        Cust[] array1 = new Cust[100];  //支出
+        int[] budget = new int[12];
         int i = 0;
-
+        int j = 0;
 
         while (true) {
             menu();
@@ -120,41 +130,78 @@ public class Cust {
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
-
                     array[i] = new Cust();
                     array[i].record_Get(sc, array,i);
                     i++;
                     break;
 
-
                 case 2:
-                    array[i] = new Cust();
-                    array[i].record_output(sc,array,i);
-                    i++;
+                    array1[j] = new Cust();
+                    array1[j].record_output(sc,array1,j);
+                    j++;
                     break;
 
                 case 3:
-                    if (i == 0) {
+                    if (i == 0 && j == 0) {
                         System.out.println("没有记录可显示。");
                     } else {
-                        for (int j = 0; j < i; j++) {
-                            array[j].print(array, i);
+                        System.out.println("收入记录");
+                        for (int k = 0; k < i; k++) {
+                            array[k].print(array, i);
+                        }
+                        System.out.println("支出记录：");
+                        for (int k = 0; k < j; k++) {
+                            array1[k].print(array1, j);
                         }
                     }
                     break;
 
                 case 4:
-		    //账单查询：用户可以按照指定日期、日期范围或类别查询账单，收入和支出分开显示。
+                    //账单查询：用户可以按照指定日期、日期范围或类别查询账单，收入和支出分开显示。
                     System.out.print("请输入日期（YYYY-MM-DD）：");
                     String date = sc.next();
-                    for (int j = 0; j < i; j++) {
-                        if(date.equals(array[j].time)){
-                            array[j].print(array, i);
+                    for (int k = 0; k < i; k++) {
+                        if(date.equals(array[k].time)){
+                            array[k].print(array, i);
                         }
-                    }    
-	    }
+                    }
+                    for (int k = 0; k < j; k++) {
+                        if(date.equals(array1[k].time)){
+                            array1[k].print(array1, j);
+                        }
+                    }
+                    break;
+                    //月度预算
+                    //5.预算管理:用户可以设置每个月支出的预算限额，系统显示当前剩余可用预算。
+                    //6.月度统计:系统可以统计每月的总收入、总支出和各类别的金额。
+                case 5:
+                    System.out.print("请输入月份：");
+                    int months = sc.nextInt();
+                    System.out.print("请输入这个月的支出限额：");
+                    budget[months-1] = sc.nextInt();
+                    for (int k = 0; k < j; k++) {
+                        if(array1[k].months+1 == months) {
+                            budget[months-1] -= array1[k].money;
+                        }
+                    }
+                    System.out.println("本月剩余可用预算：" + budget[months-1]);
+                    break;
+
+                    //月度统计
+                case 6:
+                    for (int z = 1; z <= 12; z++) {
+                        System.out.println("第" + z + "月的总收入：" + income_month[z-1]);
+                        System.out.println("第" + z + "月的总支出：" + output_month[z-1]);
+                    }
+                    break;
+
+                case 7:
+                    exit(0);
+            }
         }
     }
 }
+
+
 
 
